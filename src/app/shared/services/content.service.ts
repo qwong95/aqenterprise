32b89ea34840f7b7a79bd04fdac26fe0b5c3985f
qwong95 <qwong1995@gmail.com>
2018-09-09 19:23:41 -0700

    Changed some images

--- src/app/shared/services/content.service.ts
+++ src/app/shared/services/content.service.ts
@@ -5,8 +5,8 @@ import { Injectable } from '@angular/core';
 })
 export class ContentService {
   pages: Object = {
-    'home': {title: 'Home', subtitle: 'Welcome Home!', content: 'Some home content.', image: 'assets/billboard-notary.jpg'},
-    'about': {title: 'About', subtitle: 'About Us', content: 'AQ Enterprise is a notary and interpreting company.', image: 'assets/billboard-notary.jpg'},
-    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'Mobile: (626) 322-7518.', image: 'assets/billboard-notary.jpg'}
+    'home': {title: 'Home', subtitle: 'Welcome to the homepage of AQ Enterprise!', content: 'Need service? You have come to the right place.', image: 'assets/fingerprint3.png'},
+    'about': {title: 'About', subtitle: 'About Us', content: 'AQ Enterprise provides services for fingerprinting, notary, and interpretations. Interpretations are available in Spanish, Cantonese, and Mandarin.', image: 'assets/billboard-notary.jpg'},
+    'contact': {title: 'Contact', subtitle: 'Contact Us', content: 'Mobile: (626) 322-7518 \n Email: aqbusiness9395@gmail.com', image: 'assets/billboard-notary.jpg'}
   };
 }
